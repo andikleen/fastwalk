f2fcceef199fd0757326e22a6d87a9760eb0c218
Andi Kleen <ak@linux.intel.com>
2013-09-23 23:05:46 -0700

    update copyright

--- fastwalk.c
+++ fastwalk.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 by Intel Corp.
+/* Copyright (c) 2010-2013 by Intel Corp.
 
    fastwalk is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
